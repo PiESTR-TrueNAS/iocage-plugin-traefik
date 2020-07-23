369dde83827df500e565fb6b9e4065917f785eef
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-23 23:07:02 +0200

    oops

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 	
 # Setup services
-iocage exec "$1" sysrc "traefik_conf=/config/traefik.toml"
-iocage exec "$1" sysrc "traefik_enable=YES"
-iocage exec "$1" sysrc "firewall_script=/config/firewall.rules"
-iocage exec "$1" sysrc "firewall_enable=YES"
+sysrc "traefik_conf=/config/traefik.toml"
+sysrc "traefik_enable=YES"
+sysrc "firewall_script=/config/firewall.rules"
+sysrc "firewall_enable=YES"
